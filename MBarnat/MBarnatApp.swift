d77b22daf0664c220b44850c00193ae86c88b90e
Michal Barnat <michalbarnat@outlook.com>
2022-08-06 12:19:24 +0200

    Initial Commit

---
+++ MBarnat/MBarnatApp.swift
@@ -0,0 +1,17 @@
+//
+//  MBarnatApp.swift
+//  MBarnat
+//
+//  Created by Michał Barnat on 06/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct MBarnatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
